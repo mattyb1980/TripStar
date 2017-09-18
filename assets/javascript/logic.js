ebd5c9f116dfe0010c2a0da8f00e5ac4deffc2a1
Matthew Beebe <matthewbeebe@Matthews-MacBook-Pro.local>
2017-09-18 14:26:02 -0400

    Trying to get google maps to show

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -14,10 +14,14 @@
 
   // GoogleMaps apiKey
   // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyB9Nvofo-afipjBXoaO4g_hoyDMsIZqUiE
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
+	function initMap() {
+	        var uluru = {lat: -25.363, lng: 131.044};
+	        var map = new google.maps.Map(document.getElementById('map'), {
+	          zoom: 4,
+	          center: uluru
+	        });
+	        var marker = new google.maps.Marker({
+	          position: uluru,
+	          map: map
+	        });
+	      }
