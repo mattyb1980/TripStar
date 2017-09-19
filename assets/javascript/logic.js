36de8e4f066f5d5b7453bcdb606d844e5cb30645
Matthew Beebe <matthewbeebe@Matthews-MacBook-Pro.local>
2017-09-19 10:40:48 -0400

    Working towards a map that takes input form user

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,26 +2,82 @@
 
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDF9Vvwg15OjwYLYFbGCZwWHJuG4KqA_Y8",
-    authDomain: "tripstar-94b87.firebaseapp.com",
-    databaseURL: "https://tripstar-94b87.firebaseio.com",
-    projectId: "tripstar-94b87",
-    storageBucket: "",
-    messagingSenderId: "8807954263"
+   apiKey: "AIzaSyAJt3fgxtDdHMSi3D_UVm-AGh7--BUX-lU",
+   authDomain: "tripstar-1505508268662.firebaseapp.com",
+   databaseURL: "https://tripstar-1505508268662.firebaseio.com",
+   projectId: "tripstar-1505508268662",
+   storageBucket: "tripstar-1505508268662.appspot.com",
+   messagingSenderId: "831676656189"
   };
   
   firebase.initializeApp(config);
 
   // GoogleMaps apiKey
   // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyB9Nvofo-afipjBXoaO4g_hoyDMsIZqUiE
-	function initMap() {
-	        var uluru = {lat: -25.363, lng: 131.044};
-	        var map = new google.maps.Map(document.getElementById('map'), {
-	          zoom: 15,
-	          center: uluru
-	        });
-	        var marker = new google.maps.Marker({
-	          position: uluru,
-	          map: map
-	    });
-	}
+	function initAutocomplete() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13,
+          mapTypeId: 'roadmap'
+        });
+
+        // Create the search box and link it to the UI element.
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
+
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+      }
+	
